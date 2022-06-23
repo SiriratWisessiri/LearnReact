3c2dc4cd69cad205772f9f3f9a03102ff9bcc485
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-06-23 10:31:28 +1000

    Initial commit for React Static Page

---
+++ ReactStaticPage/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import logo from './reactLogo.png'
+
+function App() {
+  return (
+    <div className="">
+      <nav>
+        <img src={logo}></img>
+      </nav>
+      <h1>Fun Fact about React</h1>
+  
+      <ol>
+        <li>Was first release in2013</li>
+        <li>Was originally create by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub </li>
+        <li>Is maintained by Facebook</li>
+      </ol>
+     
+    </div>
+  );
+}
+
+export default App;
