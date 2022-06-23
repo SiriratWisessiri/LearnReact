ae7af9aa6094e052965998af4e2543d7cfe88bd9
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-06-23 12:47:14 +1000

    Learn parent and child relationship

--- ReactStaticPage/src/App.js
+++ ReactStaticPage/src/App.js
@@ -5,19 +5,42 @@ import logo from './reactLogo.png'
 function App() {
   return (
     <div className="">
-      <nav>
-        <img src={logo}></img>
-      </nav>
-      <h1>Fun Fact about React</h1>
-  
+      <Header />
+      <Maincontent />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+    <nav>
+      <img src={logo}></img>
+    </nav>
+  </header>
+  );
+}
+
+const Maincontent =()=> {
+  return(
+    <div>
+      <h1>Fun Fact about React</h1> 
       <ol>
         <li>Was first release in2013</li>
         <li>Was originally create by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub </li>
         <li>Is maintained by Facebook</li>
       </ol>
-     
     </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2022 Siri. All right reserved.</p>
+    </footer>
   );
 }
 
