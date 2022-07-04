911bdb1fc8eb27fcec891dec1e31c0a3eb84db3e
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-07-04 14:41:00 +1000

    Learn prevState

--- dynamic-webpage/src/Ternary.js
+++ dynamic-webpage/src/Ternary.js
@@ -1,6 +1,8 @@
 
 import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import {useState} from 'react';
+
 
 function Ternary() {
     /**
@@ -9,19 +11,30 @@ function Ternary() {
      * 
      * Hint: you will no longer need the `answer` variable
      */
-    const isGoingOut = false
-    
+
+    //  const isGoingOut = false
+
+        /**
+     * Challenge: 
+     * - Initialize state for `isGoingOut` as a boolean
+     * - Make it so clicking the div.state--value flips that
+     *   boolean value (true -> false, false -> true)
+     * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+     */
+    const[isGoingOut, setIsgoingOut] = useState(true)
+    function handleIsgoingOut () {
+       setIsgoingOut(prevState => !prevState)
+    }
     return (
         <div>
             <Card className="text-center">
                 <Card.Header>Learn Tenary Operation</Card.Header>
                 <Card.Body>
                     <Card.Title>Do I feel like going out tonight?</Card.Title>
-                    <Badge bg="info">{isGoingOut ? "Yes" : "No"}</Badge>
+                    <Button bg="info" onClick={handleIsgoingOut}>{isGoingOut ? "Yes" : "No"} </Button>
                 </Card.Body>
             </Card>
-        </div>
-        
+        </div>      
     )
 }
 
