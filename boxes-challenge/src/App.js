6dfacaa3cccbb64b09c03e65d1151b1c92305529
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-07-05 12:15:26 +1000

    Learn how to use JS style and set condition for style change

--- boxes-challenge/src/App.js
+++ boxes-challenge/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { useState } from 'react';
 import Boxes from './boxes';
 
-function App() {
+
+function App(props) {
   //  Challenge part 1:
   //  1. Initialize state with the default value of the
   //     array pulled in from boxes.js
@@ -11,8 +12,17 @@ function App() {
   // 2. Map over that state array and display each one
   //as an empty square (black border, transparent bg color)
   //(Don't worry about using the "on" property yet)
-  const boxElements = box.map(box => (  // this "box" is from the box state. "box" = initial use state of "Boxes" array
-    <div className='box' key={box.id}> 
+
+  //This is JS style instead of CSS. The benefit of this is so you can target the JS condition to change the style.
+  // We will set the dark mode in index.js and giving conditon if it's dark mode then will display different color
+  // We use Darkmode as a props so need to pass props in as a parameter!
+  const styles = {
+    backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+}
+  const boxElements = box.map(box => (  // this "box" is from the box state. "box" = initial use state of "Boxes" array. And when using .map or working with array need to have uniqe key to avoid error
+    <div className='box' style={styles} key={box.id}> 
+    {/* We can use JS style to style our elements "style={styles}" instead of CSS. The benefit of this is so you can target the JS condition to change the style*/}
+    {/* This will render all the boxes as it's .map through each box in Boxes array*/}
     </div>
   )) 
   return (
