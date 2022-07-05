3e27dcf4b66d8b5fb741c9918f42e745dacd0910
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-07-05 14:08:39 +1000

    Learn to set new state for onclick function to toggle the box style

--- boxes-challenge/src/Box.js
+++ boxes-challenge/src/Box.js
@@ -1,9 +1,29 @@
+import {useState} from 'react'    
+    /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+
+
 function Box (props) {
+    const [onOff, setOnOff] = useState(props.on)
+    
     const boxStyle = {
-        backgroundColor: props.on ? "#222222" : "none"
+        backgroundColor: onOff ? "#222222" : "transparent"
     }
+
+    function toggle() {
+        setOnOff(prevState => !prevState)
+    }
+
     return (
-        <div className='box' style={boxStyle}></div>
+        <div className='box' style={boxStyle} onClick={toggle}></div>
     )   
 }
 
