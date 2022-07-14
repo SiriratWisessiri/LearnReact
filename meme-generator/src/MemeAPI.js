6efed96ceafc51f78c7045c92bdf8c6e7ca84843
Sirirat Wisessiri <sirirat.wisessiri@avanade.com>
2022-07-14 13:02:32 +1000

    Learn use asynce function to get data from API

--- meme-generator/src/MemeAPI.js
+++ meme-generator/src/MemeAPI.js
@@ -37,11 +37,33 @@ function MemeAPI() {
 
     //  As soon as the Meme component loads the first time,
     //  make an API call to "https://api.imgflip.com/get_memes".
+
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
         .then(rest => rest.json())
         .then(APIdata => setAllMemeImages(APIdata.data.memes))
     }, [])
+//////
+    // Or you can use Async funtion to get the data
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+
+    // useEffect(() => {
+    //     async function getMemes() {
+    //         const res = await fetch("https://api.imgflip.com/get_memes")
+    //         const APIdata = await res.json()
+    //         setAllMemeImages(APIdata.data.memes)
+    //     }
+    //     getMemes()
+    // }, [])
+//////
 
     function getMemeImage() {
     //    const memesArray = allMemeImages.data.memes
